9a44f5a78a3715e5e1af09eb9bd99cae6c727e5a
Samarth Krishna Joshi <skrishnaj86@gmail.com>
2021-08-17 16:37:35 +0530

    feat: progress

--- app.js
+++ app.js
@@ -1,46 +1,69 @@
 const billAmount = document.querySelector("#bill-amount");
-const cashGiven =  document.querySelector("#cash-given");
+const cashGiven = document.querySelector("#cash-given");
+
 const checkButton = document.querySelector("#check-btn");
 const msgDisplay = document.querySelector("#msg-display");
 
+const nextButton = document.querySelector("#next-btn");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
-const availableNotes = [2000,500,100,20,10,5,1];
+const returnChangeDiv = document.querySelector("#returnChangeDiv");
 
-function minimiseReturnAmount(returnAmount){
-    for(i=0;i<availableNotes.length;++i){
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+nextButton.addEventListener("click", function nextButtonJob() {
+    hideMessage();
+    if (billAmount.value > 0) {
+        cashGivenDiv.style.display = "block";
+        nextButton.style.display = "none";
+    } else if (billAmount.value <= 0) {
+        hideall();
+        showMessage("Enter Valid Bill");
+    }
+   
+});
+
+function minimiseReturnAmount(returnAmount) {
+    returnChangeDiv.style.display = "block";
+    for (i = 0; i < availableNotes.length; ++i) {
         const minNote = Math.trunc(returnAmount / availableNotes[i]);
         noOfNotes[i].innerText = minNote;
         console.log(minNote);
         returnAmount = returnAmount % availableNotes[i];
-        
+
     }
 }
 
- checkButton.addEventListener("click", function validateCashandBill(){
+checkButton.addEventListener("click", function validateCashandBill() {
     hideMessage();
-    if(billAmount.value>0){ 
-            if(cashGiven.value >= billAmount.value ){
-                const returnAmount = cashGiven.value - billAmount.value;
-                minimiseReturnAmount(returnAmount);
-            }
-    } else{
-         if(billAmount.value<=0){ 
-            showMessage("Invalid BILL, Please Enter value Greater than 0");
-         } 
-         else if(isNaN(billAmount.value))
-            showMessage("The i/p is Not a Number");
-         else if(cashGiven.value<billAmount.value){
-                showMessage("Congratulations for becoming DishWasher today, #HappyOnboarding");
-            }
-     }
- });
+    
+    if( Number(cashGiven.value) >= Number(billAmount.value) ) {
+        const returnAmount = cashGiven.value - billAmount.value;
+        minimiseReturnAmount(returnAmount);
+        console.log(returnAmount);
+    }
+   
+    else if (Number(cashGiven.value) <= 0) {
 
- function showMessage(msg){
+        showMessage("Thanks for the Imaginary currency but we don't do that here ")
+
+    } 
+    else{ 
+        showMessage("Congratulations for becoming DishWasher today, #HappyOnboarding");
+    }
+    
+});
+
+function showMessage(msg) {
     msgDisplay.style.display = "block";
-    msgDisplay.innerText= msg;
- }
+    msgDisplay.innerText = msg;
+}
 
- function hideMessage(){
-     msgDisplay.style.display = "none";
- }
+function hideMessage() {
+    msgDisplay.style.display = "none";
+}
+
+function hideall(){
+    cashGivenDiv.style.display = "none";
+    returnChangeDiv.style.display = "none";
+}
